3cb58b9301115b78f4f3dcaa284df16574aff176
tubisiowyspiwor <tubisiowyspiwor@gmail.com>
2019-01-19 20:08:49 +0100

    Update autorefresh.js

--- autorefresh.js
+++ autorefresh.js
@@ -2,4 +2,4 @@ setInterval(function() {
     if (hero.hp > 1) {
         window.location.reload();
     }
-}, Math.floor(Math.random() * 360000) + 420000);
+}, Math.floor(Math.random() * 360000) + 420000)
